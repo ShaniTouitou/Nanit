4a57faa206a1aeef8ab1d32456c6b292ad56a737
ShaniTouitou <touitoushani@gmail.com>
2025-08-03 22:55:05 +0300

    Fix the sizes

--- app/src/main/java/com/example/nanit/screens/BirthdayScreen.kt
+++ app/src/main/java/com/example/nanit/screens/BirthdayScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -141,176 +142,199 @@ fun BirthdayScreen(
         ) {
             assets.backgroundImg()
 
-            ConstraintLayout(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 20.dp, vertical = 20.dp)
-            ) {
-                val (title, swirlsRow, ageLabel, babyBox, logo) = createRefs()
+            BoxWithConstraints {
+                val screenHeight = maxHeight
+                val dynamicTopPadding = screenHeight * 0.15f
 
-                Box(
+                ConstraintLayout(
                     modifier = Modifier
-                        .constrainAs(title) {
-                            top.linkTo(parent.top)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                        },
-                    contentAlignment = Alignment.Center
+                        .fillMaxSize()
+                        .padding(
+                            start = 20.dp,
+                            end = 20.dp,
+                            top = dynamicTopPadding,
+                            bottom = 20.dp
+                        )
                 ) {
-                    if (swirlsWidth > 0) {
-                        val textWidth = with(density) { swirlsWidth.toDp() }
+                    val (title, swirlsRow, ageLabel, babyBox, logo) = createRefs()
 
-                        Text(
-                            text = "$DISPLAY_TEXT_TODAY ${data.name.uppercase()} $DISPLAY_TEXT_IS",
-                            fontSize = 21.sp,
-                            color = TextColor,
-                            fontFamily = BentonSans,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            maxLines = Int.MAX_VALUE,
-                            letterSpacing = (-0.42).sp,
-                            softWrap = true,
-                            modifier = Modifier.width(textWidth)
+                    Box(
+                        modifier = Modifier
+                            .constrainAs(title) {
+                                top.linkTo(parent.top)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (swirlsWidth > 0) {
+                            val textWidth = with(density) { swirlsWidth.toDp() }
+
+                            Text(
+                                text = "$DISPLAY_TEXT_TODAY ${data.name.uppercase()} $DISPLAY_TEXT_IS",
+                                fontSize = 21.sp,
+                                color = TextColor,
+                                fontFamily = BentonSans,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                                maxLines = Int.MAX_VALUE,
+                                letterSpacing = (-0.42).sp,
+                                softWrap = true,
+                                modifier = Modifier.width(textWidth)
+                            )
+                        }
+                    }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .onGloballyPositioned { coordinates ->
+                                swirlsWidth = coordinates.size.width
+                            }
+                            .constrainAs(swirlsRow) {
+                                top.linkTo(title.bottom, margin = 13.dp)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.left_swirls_ic),
+                            contentDescription = null
+                        )
+
+                        Spacer(Modifier.width(22.dp))
+
+                        Image(
+                            painter = painterResource(id = getNumberIconRes(ageMonths)),
+                            contentDescription = null
+                        )
+
+                        Spacer(Modifier.width(22.dp))
+
+                        Image(
+                            painter = painterResource(R.drawable.right_swirls_ic),
+                            contentDescription = null
                         )
                     }
-                }
 
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .onGloballyPositioned { coordinates ->
-                            swirlsWidth = coordinates.size.width
-                        }
-                        .constrainAs(swirlsRow) {
-                            top.linkTo(title.bottom, margin = 13.dp)
+                    Text(
+                        text = displayText,
+                        fontSize = 18.sp,
+                        color = TextColor,
+                        fontFamily = BentonSans,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = (-0.36).sp,
+                        modifier = Modifier.constrainAs(ageLabel) {
+                            top.linkTo(swirlsRow.bottom, margin = 14.dp)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                         }
-                ) {
-                    Image(painter = painterResource( R.drawable.left_swirls_ic), contentDescription = null)
+                    )
 
-                    Spacer(Modifier.width(22.dp))
+                    val babyFaceSizeDp = 200.dp
 
-                    Image(painter = painterResource(id = getNumberIconRes(ageMonths)), contentDescription = null)
+                    Box(
+                        modifier = Modifier
+                            .constrainAs(babyBox) {
+                                top.linkTo(ageLabel.bottom, margin = 15.dp)
+                                start.linkTo(parent.start)
+                                end.linkTo(parent.end)
+                            }
+                            .padding(horizontal = 50.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        val customImageUri = viewModel.babyFaceUri.value
+                        val isCustomImage = customImageUri != null
+                        val customImagePainter =
+                            customImageUri?.let { rememberAsyncImagePainter(it) }
 
-                    Spacer(Modifier.width(22.dp))
+                        val innerFaceSizeDp = babyFaceSizeDp - 10.dp
+                        val density = LocalDensity.current
+                        val offsetDp = with(density) {
+                            val radiusPx = babyFaceSizeDp.toPx() / 2f
+                            val offsetPx = radiusPx * 0.707f
+                            offsetPx.toDp()
+                        }
 
-                    Image(painter = painterResource(R.drawable.right_swirls_ic), contentDescription = null)
-                }
+                        Image(
+                            painter = painterResource(id = assets.themeFaceImg),
+                            contentDescription = "Themed baby face background",
+                            modifier = Modifier
+                                .size(babyFaceSizeDp)
+                                .clip(CircleShape)
+                        )
 
-                Text(
-                    text = displayText,
-                    fontSize = 18.sp,
-                    color = TextColor,
-                    fontFamily = BentonSans,
-                    fontWeight = FontWeight.Bold,
-                    letterSpacing = (-0.36).sp,
-                    modifier = Modifier.constrainAs(ageLabel) {
-                        top.linkTo(swirlsRow.bottom, margin = 14.dp)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
+                        Image(
+                            painter = customImagePainter
+                                ?: painterResource(id = assets.babyFaceImg),
+                            contentDescription = if (isCustomImage) "User baby face" else "Default baby face",
+                            modifier = Modifier
+                                .size(innerFaceSizeDp)
+                                .clip(CircleShape)
+                                .clickable { showDialog = true },
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Image(
+                            painter = painterResource(id = assets.camImg),
+                            contentDescription = "Camera icon",
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .offset(x = offsetDp, y = -offsetDp)
+                                .clickable { showDialog = true }
+                        )
                     }
-                )
 
-                val babyFaceSizeDp = 200.dp
-
-                Box(
-                    modifier = Modifier
-                        .constrainAs(babyBox) {
-                            top.linkTo(ageLabel.bottom, margin = 15.dp)
+                    Image(
+                        painter = painterResource(R.drawable.nanit_ic),
+                        contentDescription = null,
+                        modifier = Modifier.constrainAs(logo) {
+                            top.linkTo(babyBox.bottom, margin = 15.dp)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
                         }
-                        .padding(horizontal = 50.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    val customImageUri = viewModel.babyFaceUri.value
-                    val isCustomImage = customImageUri != null
-                    val customImagePainter = customImageUri?.let { rememberAsyncImagePainter(it) }
-
-                    val innerFaceSizeDp = babyFaceSizeDp - 10.dp
-                    val density = LocalDensity.current
-                    val offsetDp = with(density) {
-                        val radiusPx = babyFaceSizeDp.toPx() / 2f
-                        val offsetPx = radiusPx * 0.707f
-                        offsetPx.toDp()
-                    }
-
-                    Image(
-                        painter = painterResource(id = assets.themeFaceImg),
-                        contentDescription = "Themed baby face background",
-                        modifier = Modifier
-                            .size(babyFaceSizeDp)
-                            .clip(CircleShape)
-                    )
-
-                    Image(
-                        painter = customImagePainter ?: painterResource(id = assets.babyFaceImg),
-                        contentDescription = if (isCustomImage) "User baby face" else "Default baby face",
-                        modifier = Modifier
-                            .size(innerFaceSizeDp)
-                            .clip(CircleShape)
-                            .clickable { showDialog = true },
-                        contentScale = ContentScale.Crop
-                    )
-
-                    Image(
-                        painter = painterResource(id = assets.camImg),
-                        contentDescription = "Camera icon",
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .offset(x = offsetDp, y = -offsetDp)
-                            .clickable { showDialog = true }
                     )
                 }
-
-                Image(
-                    painter = painterResource(R.drawable.nanit_ic),
-                    contentDescription = null,
-                    modifier = Modifier.constrainAs(logo) {
-                        top.linkTo(babyBox.bottom, margin = 15.dp)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-                )
+            }
+        } ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Button(onClick = onConnect) {
+                Text(BTN_TEXT_CONNECT)
             }
         }
-    } ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Button(onClick = onConnect) {
-            Text(BTN_TEXT_CONNECT)
+
+        // region Dialog
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text(DIALOG_TEXT_SELECT_OPTION) },
+                text = { Text(DIALOG_TEXT_CHOICE) },
+                confirmButton = {
+                    TextButton(onClick = {
+                        showDialog = false
+                        val uri = createImageUri(context)
+                        if (uri != null) {
+                            cameraImageUri = uri
+                            cameraLauncher.launch(uri)
+                        }
+                    }) {
+                        Text(DIALOG_TEXT_CHOOSE_PICTURE)
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showDialog = false
+                        galleryLauncher.launch("image/*")
+                    }) {
+                        Text(DIALOG_TEXT_CHOOSE_GALLERY)
+                    }
+                }
+            )
         }
     }
 
-    // region Dialog
-
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text(DIALOG_TEXT_SELECT_OPTION) },
-            text = { Text(DIALOG_TEXT_CHOICE) },
-            confirmButton = {
-                TextButton(onClick = {
-                    showDialog = false
-                    val uri = createImageUri(context)
-                    if (uri != null) {
-                        cameraImageUri = uri
-                        cameraLauncher.launch(uri)
-                    }
-                }) {
-                    Text(DIALOG_TEXT_CHOOSE_PICTURE)
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = {
-                    showDialog = false
-                    galleryLauncher.launch("image/*")
-                }) {
-                    Text(DIALOG_TEXT_CHOOSE_GALLERY)
-                }
-            }
-        )
-    }
     // endregion
+
 }
 
 // endregion
