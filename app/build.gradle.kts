0386bba9d390e1743b28d1d8681176f5a093f8ef
ShaniTouitou <touitoushani@gmail.com>
2025-08-03 12:28:53 +0300

    Add all the needed icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
